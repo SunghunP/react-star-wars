f62a6d970a1da0f4cb61b01e47ed43eb7491e936
Sunghun Park <sunghun.josephp@gmail.com>
2022-05-11 19:51:36 -0700

    Create function called getStarships

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+
+  // function that can be called to fetch all of the data
+  const getStarships = async () => { 
+    // make api call
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
