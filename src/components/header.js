06991bfe855ed24fc76a6d13aab16d89840380ef
Joe Park <sunghun.josephp@gmail.com>
2022-05-11 20:55:50 -0700

    Add styled-components to dependencies

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,8 @@
+import styled from "styled-components"
+
 const Header = () => {
+  const styles @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Fredoka&display=swap');
+
   return <header>STAR WARS STARSHIPS</header>;
 };
 
