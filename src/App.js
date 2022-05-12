d71f37e3c5bd588eacf4b705166028c91f6c67e3
Sunghun Park <sunghun.josephp@gmail.com>
2022-05-11 20:26:12 -0700

    Add functionality for getStarships function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [starships, setStarships] = useState(null);
+
   // function that can be called to fetch all of the data
   const getStarships = async () => {
     // make api call
+    const response = await fetch(`https://swapi.dev/api/starships`);
+    // convert response to js object
+    const starshipsResponse = await response.json();
+    //update the state to the object of starships
+    setStarships(starshipsResponse.results);
   };
 
-  return <div className="App"></div>;
+  // will run once or if value in array is changed
+  useEffect(() => {
+    getStarships();
+  }, []);
+
+  console.log(starships);
+
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+    </div>
+  );
 }
 
 export default App;
